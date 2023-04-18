7c61d7c692e988cc8a0da7cdc571dd6a32add798
carlos738 <carlosasl135@hotmail.com>
2023-04-18 23:33:03 +0000

    Add files via upload

---
+++ new-post.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-post',
+  templateUrl: './new-post.component.html',
+  styleUrls: ['./new-post.component.css']
+})
+export class NewPostComponent implements OnInit {
+tweetMessage: string =""
+tweetMaxLength: number =240
+tweetRemain: number = this.tweetMaxLength
+
+sendTweet(){
+  alert(this.tweetMessage)
+}
+changeTweet(){
+  this.tweetRemain=240 - this.tweetMessage.length
+}
+
+constructor() { }
+
+ngOnInit() {
+}
+
+}
